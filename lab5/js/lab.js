ee0ff3120d3f8e43fc40a41838a4d194192eb5c2
Leonardo Sanchez <leosanchez@csumb.edu>
2022-02-09 15:22:10 -0800

    Lab 4 and Lab 5

---
+++ lab5/js/lab.js
@@ -0,0 +1,22 @@
+/**
+ * Author:    Leonardo Sanchez
+ * Created:   2.09.2022
+ *
+ * (c) Copyright by Blub Corp.
+ **/
+
+ // Define Variables
+ make = "Ford";
+ model = "Taurus";
+ color = "Red";
+ year = 1995;
+
+ // calculate
+ age = 2022 - year;
+
+ // output
+ document.writeln("Make: " + make + "<br>");
+ document.writeln("Model: " + model + "<br>");
+ document.writeln("Color: " + color + "<br>");
+ document.writeln("Year: " + year + "<br>");
+ document.writeln("Age: " + age + " years<br>");
