3029c3b1d9318e27f757bc47147fdb01ffa2f7a1
Leonardo Sanchez <leosanchez@csumb.edu>
2022-02-28 12:46:15 -0800

    HTML Update

---
+++ lab9/js/lab.js
@@ -0,0 +1,12 @@
+/**
+ * Author:    Leonardo Sanchez
+ * Created:   2.09.2022
+ *
+ * (c) Copyright by Blub Corp.
+ **/
+
+var a = [2000,40,6000,55,20,40,70,1.5,0.5]
+
+var b = a.map(function (x) {return x + 2
+});
+document.write(b);
